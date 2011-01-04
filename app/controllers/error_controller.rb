22e7baf6e42de087551581135487a24283dff165
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-01-04 01:42:18 +0100

    Made sure the host is added to the url

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -1,6 +1,6 @@
 class ErrorController < ApplicationController
   def handle404
-    @url_that_was_not_found = request.env['REQUEST_URI']
+    @url_that_was_not_found = "#{request.scheme}://#{request.host}#{request.path_info}"   
     render :template => "/error/handle404.html.haml", :status => 404
   end
 
