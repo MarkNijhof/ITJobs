2a655041e2cc068f5626137db19e3d289fc1ee4f
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-01-10 03:44:38 +0100

    added the Alhola editor to handle job posting texts

---
+++ public/javascripts/aloha.js
@@ -0,0 +1,10 @@
+GENTICS.Aloha.settings = {
+	logLevels: {'error': true, 'warn': true, 'info': true, 'debug': true},
+	errorhandling : false,
+	ribbon: false,	
+	"plugins": {
+		"com.gentics.aloha.plugins.Format": {
+			config: ["b","i","del","sub","sup","p","h1","h2","h3","removeFormat"]
+		}
+  	}
+};
