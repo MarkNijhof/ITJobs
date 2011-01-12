6fc9263fb25d0a5d4c9caec456158706a49edec5
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-01-12 02:57:15 +0100

    some removal of commented code

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -6,21 +6,11 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 CKEDITOR.editorConfig = function( config )
 {
   config.PreserveSessionOnFileBrowser = true;
-  // Define changes to default configuration here. For example:
-  config.language = 'en';
-  //config.uiColor = '#FFFFFF';
 
-  //config.ContextMenu = ['Generic','Anchor','Flash','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','Table','Form'] ; 
-  
-  //config.height = '400px';
-  //config.width = '100%';
+  config.language = 'en';
   
   config.resize_enabled = false;
   config.toolbarCanCollapse = false;
-  //config.resize_maxHeight = 2000;
-  //config.resize_maxWidth = 750;
-  
-  //config.startupFocus = true;
   
   // works only with en, ru, uk languages
   config.extraPlugins = "autogrow";
@@ -28,7 +18,6 @@ CKEDITOR.editorConfig = function( config )
   config.removePlugins = 'resize';
   
   config.toolbar = 'Easy';
-  //config.editor.mode = 'wysiwyg';
   
   config.toolbar_Easy =
     [
@@ -43,8 +32,8 @@ CKEDITOR.editorConfig = function( config )
     ];
   config.contentsCss = '/stylesheets/compiled/screen.css';
   config.bodyClass = 'content';
-//  config.skin = 'itjobs';
   config.format_tags = 'p;h1;h2;h3';
+
   config.sharedSpaces = {
 	top: "topSpace",
 	bottom: "bottomSpace"
