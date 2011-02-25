74d0e1b4476dcc3855e2e38159ff33ac98677fba
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-02-25 22:30:29 +0100

    moved away from jQuery to prototype, mostly to be able to use the original wysihat editor, if I need jQuery later then I can add that again

--- public/javascripts/employer_job.js
+++ public/javascripts/employer_job.js
@@ -1,43 +1,41 @@
 
-$(document).ready(function(){
-
-	$(window).scroll(function () {
+document.observe("dom:loaded", function() {
+	Event.observe(window, 'scroll', function() {
 		var scrollTop = 116;
 		var topSpaceTop = '14px'
 
-		if ($(window).scrollTop() > scrollTop) {
-			$('.editor_toolbar').css('position', 'fixed');
-			$('.editor_toolbar').css('top', topSpaceTop);
+		if (document.viewport.getScrollOffsets()[1] > scrollTop) {
+			$$('div.editor_toolbar')[0].setStyle({
+				position: 'fixed',
+				top: topSpaceTop
+			});
 		} else {
-			$('.editor_toolbar').css('position', 'absolute');
-			$('.editor_toolbar').css('top', '');
+			$$('div.editor_toolbar')[0].setStyle({
+				position: 'absolute',
+				top: ''
+			});
 		}
 	});
-
-	var editor1 = WysiHat.Editor.attach($('#job_description'));
-	var editor2 = WysiHat.Editor.attach($('#company_description'));
 	
-	jQuery.extend(jQuery.expr[':'], {
-	    focus: function(element) { 
-	        return element == document.activeElement; 
-	    }
-	});
 	var isEditor1Focussed = false;
-	$("#job_description").focus(function(){ isEditor1Focussed = true; });
-	$("#company_description").focus(function(){ isEditor1Focussed = false; });
+	Event.observe('job_description', 'focus', function() { isEditor1Focussed = true; });
+	Event.observe('company_description', 'focus', function() { isEditor1Focussed = false; });
+		
+	var editor1 = WysiHat.Editor.attach('job_description');
+	var editor2 = WysiHat.Editor.attach('company_description');
 
-    var boldButton = $('.editor_toolbar .bold').first();
-    var italicButton = $('.editor_toolbar .italic').first();
-    var underlineButton = $('.editor_toolbar .underline').first();
-    var strikethroughButton = $('.editor_toolbar .strikethrough').first();
-    var numberedlistButton = $('.editor_toolbar .numberedlist').first();
-    var bulletsButton = $('.editor_toolbar .bullets').first();
+    var boldButton = $$('.editor_toolbar .bold').first();
+    var italicButton = $$('.editor_toolbar .italic').first();
+    var underlineButton = $$('.editor_toolbar .underline').first();
+    var strikethroughButton = $$('.editor_toolbar .strikethrough').first();
+    var numberedlistButton = $$('.editor_toolbar .numberedlist').first();
+    var bulletsButton = $$('.editor_toolbar .bullets').first();
 
-    boldButton.click(function(event) 			{ if (isEditor1Focussed) editor1.boldSelection(); 			else editor2.boldSelection(); return false; });
-    italicButton.click(function(event) 			{ if (isEditor1Focussed) editor1.italicSelection(); 		else editor2.italicSelection(); return false; });
-    underlineButton.click(function(event) 		{ if (isEditor1Focussed) editor1.underlineSelection(); 		else editor2.underlineSelection(); return false; });
-    strikethroughButton.click(function(event) 	{ if (isEditor1Focussed) editor1.strikethroughSelection(); 	else editor2.strikethroughSelection(); return false; });
-    numberedlistButton.click(function(event) 	{ if (isEditor1Focussed) editor1.toggleOrderedList(); 		else editor2.toggleOrderedList(); return false; });
-    bulletsButton.click(function(event) 		{ if (isEditor1Focussed) editor1.toggleUnorderedList(); 	else editor2.toggleUnorderedList(); return false; });
+	boldButton.on('click', function(event)          { if (isEditor1Focussed) editor1.boldSelection();          else editor2.boldSelection(); Event.stop(event); });
+	italicButton.on('click', function(event)        { if (isEditor1Focussed) editor1.italicSelection();        else editor2.italicSelection(); Event.stop(event); });
+	underlineButton.on('click', function(event)     { if (isEditor1Focussed) editor1.underlineSelection();     else editor2.underlineSelection(); Event.stop(event); });
+	strikethroughButton.on('click', function(event) { if (isEditor1Focussed) editor1.strikethroughSelection(); else editor2.strikethroughSelection(); Event.stop(event); });
+	numberedlistButton.on('click', function(event)  { if (isEditor1Focussed) editor1.toggleOrderedList();      else editor2.toggleOrderedList(); Event.stop(event); });
+	bulletsButton.on('click', function(event)       { if (isEditor1Focussed) editor1.toggleUnorderedList();    else editor2.toggleUnorderedList(); Event.stop(event); });
 
 });
