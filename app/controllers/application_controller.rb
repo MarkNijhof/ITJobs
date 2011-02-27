73da6b0de2997285f32bce5710277d252b6ee057
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-02-27 01:17:19 +0100

    created a redirect from itjo.bs to www.itjo.bs and also to https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :redirect_from_www
+    
+  def redirect_from_www
+    return unless ENV['production']
+    
+    redirect_to "https://www.#{request.host_with_port}#{request.path_info}" and return if /^itjo/.match(request.host)
+    
+    redirect_to "https://#{request.host_with_port}#{request.path_info}" and return if /^www/.match(request.host) && request.scheme == 'http'
+  end
 end
