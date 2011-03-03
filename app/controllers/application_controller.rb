e1682bed0359d38fc004ed190896b4b18a4ea3ab
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-03-03 02:12:01 +0100

    crap redirect stuff :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :redirect_from_www
     
   def redirect_from_www
-    return unless Rails.env.production?
+    # return unless Rails.env.production?
     
     redirect_to "https://www.#{request.host_with_port}#{request.path_info}" and return unless /^www/.match(request.host)
   end
