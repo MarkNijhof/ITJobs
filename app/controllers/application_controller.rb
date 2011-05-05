3d3eed062f8cf1478329d06be62d863e3e9b2843
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-05-06 01:05:27 +0200

    removed the www redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :redirect_to_www
-    
-  def redirect_to_www    
-    host = request.host
-    
-    return if /^www/.match(host)
-    return if /^test/.match(host)
-    return if /^staging/.match(host)
-    return if /^local/.match(host)
-    
-    redirect_to "https://www.#{request.host_with_port}#{request.path_info}"
-  end
+  # before_filter :redirect_to_www
+  #   
+  # def redirect_to_www    
+  #   host = request.host
+  #   
+  #   return if /^www/.match(host)
+  #   return if /^test/.match(host)
+  #   return if /^staging/.match(host)
+  #   return if /^local/.match(host)
+  #   
+  #   redirect_to "https://www.#{request.host_with_port}#{request.path_info}"
+  # end
 end
